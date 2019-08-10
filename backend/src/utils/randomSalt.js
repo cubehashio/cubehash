a91984f80d3c42278d2d2af3fe80c046ec17a2f5
fayazgolsangi <fayazgolsangi@gmail.com>
2019-08-11 00:08:22 +0530

    cleanup

--- backend/src/utils/randomSalt.js
+++
@@ -1,8 +0,0 @@
-import { randomBytes } from 'crypto'
-import BN from 'bn.js'
-
-export function randomSalt () {
-  const salt = new BN(`0x${randomBytes(32).toString('hex')}`)
-    // console.log('salt.toString(10):', salt.toString(10))
-  return salt.toString(10)
-}
